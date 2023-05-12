df16eba38b84cfa3bf9247ee7b8a110149715518
Mohammad Maaz Saiyed <90935281+Maaz83@users.noreply.github.com>
2023-05-12 00:38:57 -0400

    Update server.java

--- Multi-threaded Server/src/server/server.java
+++ Multi-threaded Server/src/server/server.java
@@ -1,15 +1,4 @@
-/**********************************************
- Workshop 9
 
- Course: JAC444 - 4
- Last Name: Saiyed
- First Name: Mohammad Maaz
- ID: 113485205
- Section: ZAA
- This assignment represents my own work in accordance with Seneca Academic Policy.
- Signature
- Date: 16-04-2023
- **********************************************/
 
 package server;
 
