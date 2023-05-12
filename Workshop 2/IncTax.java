88b70d45d21926a31b8e61670667847ff758784f
Mohammad Maaz Saiyed <90935281+Maaz83@users.noreply.github.com>
2023-05-12 00:42:42 -0400

    Add files via upload

---
+++ Workshop 2/IncTax.java
@@ -0,0 +1,20 @@
+/**********************************************
+Workshop 2
+
+Course: JAC444 - 4
+Last Name: Saiyed
+First Name: Mohammad Maaz
+ID: 113485205
+Section: ZAA
+This assignment represents my own work in accordance with Seneca Academic Policy.
+Signature
+Date: 17-02-2023
+**********************************************/
+
+
+public class IncTax {
+    public static void main(String[] args) {
+        IncomeTax m_tax = new IncomeTax();
+        m_tax.run();
+    }
+}
